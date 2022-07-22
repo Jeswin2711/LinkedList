c2c62ef027abb2b6a1bb35e8c840cee30fafbd64
Jeswin2711 <jeswin2711@gmail.com>
2022-07-22 12:41:24 +0530

    Added:Statement

---
+++ src/main/java/com/assignment/LinkedListMain.java
@@ -0,0 +1,11 @@
+package com.assignment;
+
+
+public class LinkedListMain
+{
+    /* Main Function to Perform All the LinkedList Operations*/
+    public static void main(String[] args) {
+        System.out.println("Welcome to LinkedList Program");
+    }
+}
+
